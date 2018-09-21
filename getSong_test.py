432acd8dfb589c8bda1d4ea72efb3da0bc6f069c
KohheiAdachi <d13301@yonago.kosen-ac.jp>
2018-09-21 16:54:06 +0900

    名前変更

--- getSong_test.py
+++ getSong_test.py
@@ -1,5 +1,5 @@
 import unittest
-from GetSong import GetSong
+from getSong import GetSong
 
 # song = GetSong()
 #
@@ -30,6 +30,7 @@ class TestGetSong(unittest.TestCase):
         kashiList = song.getSongList("https://www.uta-net.com/search/?Keyword=%E4%B9%83%E6%9C%A8%E5%9D%82&x=52&y=17&Aselect=1&Bselect=3",match="search")
         for kashiurl in kashiList:
             title,data = song.getWordofsong(kashiurl)
+
     def test_getkashi_search(self):
         song = GetSong()
         kashiList = song.getSongList("https://www.uta-net.com/artist/12550/")
